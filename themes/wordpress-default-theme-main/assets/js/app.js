33266b369c63efcd5735438b8849ef8c1e11378e
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2021-04-14 12:58:46 +0300

    faq page

--- themes/wordpress-default-theme-main/assets/js/app.js
+++ themes/wordpress-default-theme-main/assets/js/app.js
@@ -1,6 +1,6 @@
 let html = document.querySelector("html");
 
-// burger
+// BURGER MENU
 let burger = document.querySelector(".header__burger"),
   menu = document.querySelector(".header-menu");
 burger.addEventListener("click", function (e) {
@@ -8,3 +8,22 @@ burger.addEventListener("click", function (e) {
   html.classList.toggle("stop-scrolling");
   menu.classList.toggle("active");
 });
+
+// FAQ PAGE
+let qBx = document.querySelectorAll(".faq__question-box");
+for (let i = 0; i < qBx.length; i++) {
+  qBx[i].addEventListener("click", function (e) {
+    e.preventDefault;
+    if (!this.nextElementSibling.classList.contains("active")) {
+      qBx.forEach((item) => {
+        item.nextElementSibling.classList.remove("active");
+        item.children[1].classList.remove("active");
+      });
+      this.children[1].classList.add("active");
+      this.nextElementSibling.classList.add("active");
+    } else {
+      this.nextElementSibling.classList.remove("active");
+      this.children[1].classList.remove("active");
+    }
+  });
+}
---
+++ themes/wordpress-default-theme-main/templates/page-faq.php
@@ -0,0 +1,95 @@
+<?php
+/**
+* Template Name: FAQ
+*
+* @package WordPress
+* @subpackage Coelix
+* @since Coelix 1.0
+*/
+get_header();
+?>
+
+<main class="site-main">
+  <section class="faq">
+    <div class="container">
+      <?php $title = get_field( 'title_faq' );
+      if($title) { ?>
+        <h1 class="faq__title"><?= $title ?></h1>
+      <?php } ?>
+      <div class="wrapper">
+        <?php if( have_rows('faq_items') ): ?>
+          <div class="faq__box">
+            <?php 
+              while( have_rows('faq_items') ) : the_row();
+              $question = get_sub_field('question_faq');
+              $answer = get_sub_field('answer_faq');
+            ?>
+              <div class="faq__item">
+                <div class="faq__question-box">
+                  <p class="faq__question"><?= $question ?></p>
+                  <div class="faq__btn-box"></div>
+                </div>
+                <div class="faq__answer-box">
+                  <p class="faq__answer"><?= $answer ?></p>
+                </div>
+              </div>
+            <?php endwhile; ?>
+          </div>
+        <?php endif; ?>
+
+        <?php $img = get_field( 'image_faq' ); 
+        if($img) { ?>
+          <div class="faq__img-box">
+            <img src="<?= $img ?>" alt="" class="faq__img">
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </section>
+
+  <div class="faq-form">
+    <div class="container">
+      <?php $title = get_field( 'title_form_faq' );
+      if( $title ) { ?>
+        <h2 class="faq-form__title"><?= $title ?></h2>
+      <?php } ?>
+      <?php $subtitle = get_field( 'subtitle_form_faq' );
+      if( $subtitle ) { ?>
+        <p class="faq-form__subtitle"><?= $subtitle ?></p>
+      <?php } ?>
+      <div class="wrapper">
+        <?php $img = get_field( 'image_form_faq' );
+          if( $img ) { ?>
+            <div class="faq-form__img-box">
+              <img src="<?= $img ?>" alt="" class="faq-form__img">
+            </div>
+        <?php } ?>
+
+        <form action="" class="faq-form__form contact-form">
+          <div class="contact-form__item">
+            <label for="name" class="contact-form__label faq-form__label">Name</label>
+            <input type="text" class="contact-form__input faq-form__input">
+          </div>
+          <div class="contact-form__item">
+            <label for="name" class="contact-form__label faq-form__label">Tel</label>
+            <input type="tel" class="contact-form__input faq-form__input">
+          </div>
+          <div class="contact-form__item">
+            <label for="name" class="contact-form__label faq-form__label">City</label>
+            <input type="text" class="contact-form__input faq-form__input">
+          </div>
+          <div class="contact-form__item">
+            <label for="name" class="contact-form__label faq-form__label">Comment</label>
+            <textarea type="text" class="contact-form__input contact-form__input_textarea faq-form__input faq-form__input_textarea"></textarea>
+          </div>
+          <div class="contact-form__item">
+            <input type="submit" class="contact-form__submit faq-form__submit" value='<?= the_field( 'btn_form_faq' ); ?>'>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php
+get_footer();
