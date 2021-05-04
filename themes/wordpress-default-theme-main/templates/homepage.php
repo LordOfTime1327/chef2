bbf7bb0d2e7822d69f8a674bbc83bb269265f15f
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2021-05-04 23:39:33 +0300

    style categories on homepage

--- themes/wordpress-default-theme-main/templates/homepage.php
+++ themes/wordpress-default-theme-main/templates/homepage.php
@@ -37,6 +37,41 @@ get_header();
 		</div>
 	</section>
 
+	<section class="categories">
+		<div class="container">
+			<?php if ( get_field( 'categories_title' ) ) { ?>
+				<h2 class="title decor categories__title"><?= the_field( 'categories_title' ); ?></h2>
+			<?php } ?>
+			<div class="wrapper">
+				<?php 
+					$categories = get_categories([
+						'taxonomy' => 'product_cat',
+						'hide_empty' => false,
+						'orderby' => 'term_group',
+						'parent' => 0,
+					]);
+
+					foreach($categories as $category) {		
+						$thumbnail_id = get_term_meta( $category->term_id, 'thumbnail_id', true ); 
+						$image = wp_get_attachment_url( $thumbnail_id ); 
+						?>
+						
+						<div class="categories__cat-card">
+							<a href="<?= get_category_link( $category->term_id ) ?>" class="categories__cat-link">
+								<div class="categories__cat-text-box">
+									<p class="categories__cat-title"><?= $category->name ?></p>
+								</div>
+								<div class="categories__cat-img-box">
+									<img src="<?= $image ?>" alt="<?= $category->name ?>" class="categories__cat-img">
+								</div>
+							</a>
+						</div>
+
+					<?php } ?>
+			</div>
+		</div>
+	</section>
+
 	<section class="features">
 		<div class="container">
 			<?php if ( get_field( 'title_features' ) ) { ?>
@@ -61,6 +96,15 @@ get_header();
 					<?php endwhile; ?>
 				</div>
 			<?php endif; ?>
+		</div>
+	</section>
+
+	<section class="catalog">
+		<div class="container">
+			<h1>Catalog</h1>
+			<div class="wrapper">
+				<?= do_shortcode( '[products limit="4" sort_by="popularity"]' ); ?>
+			</div>
 		</div>
 	</section>
 
