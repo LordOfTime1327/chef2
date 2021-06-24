ead6bc372771c45b0b3ad507c4f14d5ce1749dde
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2021-06-24 14:32:03 +0300

    order received page

--- themes/chef/woocommerce/order/order-details-item.php
+++ themes/chef/woocommerce/order/order-details-item.php
@@ -23,10 +23,11 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 	return;
 }
 ?>
-<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'woocommerce-table__line-item order_item', $item, $order ) ); ?>">
+<tr
+  class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'woocommerce-table__line-item order_item', $item, $order ) ); ?>">
 
-	<td class="woocommerce-table__product-name product-name">
-		<?php
+  <td class="woocommerce-table__product-name product-name">
+    <?php
 		$is_visible        = $product && $product->is_visible();
 		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
 
@@ -49,11 +50,11 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 
 		do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order, false );
 		?>
-	</td>
+  </td>
 
-	<td class="woocommerce-table__product-total product-total">
-		<?php echo $order->get_formatted_line_subtotal( $item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-	</td>
+  <td class="woocommerce-table__product-total product-total">
+    <?php echo $order->get_formatted_line_subtotal( $item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+  </td>
 
 </tr>
 
@@ -61,7 +62,9 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 
 <tr class="woocommerce-table__product-purchase-note product-purchase-note">
 
-	<td colspan="2"><?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></td>
+  <td colspan="2">
+    <?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+  </td>
 
 </tr>
 
